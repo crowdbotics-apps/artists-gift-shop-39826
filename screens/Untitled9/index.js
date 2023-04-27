d7f23299e34e40835b31a0f7c674d63ece764f13
ivanapfafsovljanski <ivana.pfaf@crowdbotics.com>
2023-04-27 23:57:15 +0200

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/Untitled9/index.js
+++ screens/Untitled9/index.js
@@ -1,20 +1,48 @@
-import React from "react";
+import { useNavigation } from "@react-navigation/native";
+import { Pressable } from "react-native";
+import { Text } from "react-native";
 import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
 
 const Untitled9 = () => {
+  const navigation = useNavigation();
   return <SafeAreaView style={styles.safeArea}>
         <ScrollView contentContainerStyle={{
       backgroundColor: '#f0f0f1',
       padding: 10,
       position: 'relative',
       flex: 1
-    }}></ScrollView>
+    }}><Pressable onPress={() => {
+        navigation.navigate("Untitled10");
+      }}><Text style={styles.daGkLZLG}>{"Post"}</Text></Pressable><Pressable onPress={() => {
+        navigation.navigate("Untitled11");
+      }}><Text style={styles.JiEjtIDl}>{"Comment screen"}</Text></Pressable><Text style={styles.ICEiHwuq}>{"Comment screen"}</Text></ScrollView>
     </SafeAreaView>;
 };
 
 const styles = StyleSheet.create({
   safeArea: {
     height: '100%'
+  },
+  daGkLZLG: {
+    width: 100,
+    height: 50,
+    lineHeight: 14,
+    fontSize: 14,
+    borderRadius: 0
+  },
+  JiEjtIDl: {
+    width: 100,
+    height: 50,
+    lineHeight: 14,
+    fontSize: 14,
+    borderRadius: 0
+  },
+  ICEiHwuq: {
+    width: 100,
+    height: 50,
+    lineHeight: 14,
+    fontSize: 14,
+    borderRadius: 0
   }
 });
 export default Untitled9;
