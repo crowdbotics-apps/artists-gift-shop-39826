dc75815214a14b7b72e7e49d8046e2fe1885da8e
ivanapfafsovljanski <ivana.pfaf@crowdbotics.com>
2023-04-27 23:32:26 +0200

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "icy-wave-39826",
-  url: "https://icy-wave-39826.botics.co",
-  api: "https://icy-wave-39826.botics.co/api/v1"
-}
+  "name": "icy-wave-39826",
+  "url": "https://icy-wave-39826.botics.co",
+  "api": "https://icy-wave-39826.botics.co/api/v1",
+  "initialRoute": "Untitled1"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
